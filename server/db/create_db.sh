077a000034063796cd8d6577272507a869a1b02c
Nick Chapman <nchapman@mozilla.com>
2015-01-20 11:37:12 -0800

    fix(build): Added optional user param to create_db.sh

--- server/db/create_db.sh
+++ server/db/create_db.sh
@@ -5,14 +5,19 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # TODO convert to node when things slow down
-psql -c 'DROP DATABASE IF EXISTS chronicle;' -U postgres
-psql -c 'REASSIGN OWNED BY chronicle TO postgres;' -U postgres
-psql -c 'DROP USER IF EXISTS chronicle;' -U postgres
 
-psql -c "CREATE USER chronicle WITH PASSWORD 'chronicle';" -U postgres
-psql -c "CREATE DATABASE chronicle ENCODING 'UTF-8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';" -U postgres
-psql -c 'GRANT ALL PRIVILEGES ON DATABASE chronicle to chronicle;' -U postgres
-psql -c 'ALTER SCHEMA public OWNER TO chronicle;' -U postgres
+# Accept user for psql as first parameter but default to the current user
+# ./create_db.sh postgres
+PSQLUSER=${1-$USER}
+
+psql -c 'DROP DATABASE IF EXISTS chronicle;' -U $PSQLUSER
+psql -c "REASSIGN OWNED BY chronicle TO $PSQLUSER;" -U $PSQLUSER
+psql -c 'DROP USER IF EXISTS chronicle;' -U $PSQLUSER
+
+psql -c "CREATE USER chronicle WITH PASSWORD 'chronicle';" -U $PSQLUSER
+psql -c "CREATE DATABASE chronicle ENCODING 'UTF-8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';" -U $PSQLUSER
+psql -c 'GRANT ALL PRIVILEGES ON DATABASE chronicle to chronicle;' -U $PSQLUSER
+psql -c 'ALTER SCHEMA public OWNER TO chronicle;' -U $PSQLUSER
 psql -c "CREATE TABLE IF NOT EXISTS users (
   fxaId CHAR(32) PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
