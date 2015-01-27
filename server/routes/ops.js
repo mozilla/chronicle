64920dbc8fac74983da4683154d65cc9433241a3
Peter deHaan <peter@deseloper.com>
2015-01-26 18:01:18 -0800

    feat(heartbeat): Adding redis check to __heartbeat__

--- server/routes/ops.js
+++ server/routes/ops.js
@@ -5,18 +5,26 @@
 'use strict';
 
 var Q = require('q');
-var pg = require('pg');
 var es = require('elasticsearch');
+var pg = require('pg');
+var redis = require('redis');
 
 var config = require('../config');
 
-var dbParams = {
+var pgParams = {
   user: config.get('db.postgres.user'),
   password: config.get('db.postgres.password'),
   host: config.get('db.postgres.host'),
   port: config.get('db.postgres.port'),
   database: config.get('db.postgres.database'),
   ssl: config.get('db.postgres.ssl')
+};
+
+var redisParams = {
+  host: config.get('db.redis.host'),
+  port: config.get('db.redis.port'),
+  password: config.get('db.redis.password'),
+  database: config.get('db.redis.database')
 };
 
 // >:-(
@@ -33,7 +41,7 @@ var esParamsFactory = function() {
 
 var pgCheck = function () {
   var deferred = Q.defer();
-  pg.connect(dbParams, function(err, client, done) {
+  pg.connect(pgParams, function(err, client, done) {
     done(); // Close database connection
     if (err) {
       return deferred.reject(err);
@@ -44,14 +52,24 @@ var pgCheck = function () {
 };
 
 var esCheck = function () {
-  var deferred = Q.defer();
   var esClient = new es.Client(esParamsFactory());
-  esClient.search({
+  return esClient.ping({
     index: 'chronicle',
-    type: 'visits',
-    size: 0,
-    body: {}
-  }).then(deferred.resolve, deferred.reject).catch(deferred.reject);
+    type: 'visits'
+  });
+};
+
+var redisCheck = function () {
+  var deferred = Q.defer();
+  var client = redis.createClient(redisParams.port, redisParams.host);
+  client.on('connect', function () {
+    client.quit();
+    return deferred.resolve();
+  });
+  client.on('error', function (err) {
+    client.quit();
+    return deferred.reject(err);
+  });
   return deferred.promise;
 };
 
@@ -62,12 +80,11 @@ module.exports = [{
     handler: function (request, reply) {
       Q.all([
         pgCheck(),
-        esCheck()
-      ]).then(function (results) {
+        esCheck(),
+        redisCheck()
+      ]).then(function () {
         reply('ok').code(200);
       }, function (err) {
-        reply(err.message).code(503);
-      }).catch(function (err) {
         reply(err.message).code(503);
       });
     }
