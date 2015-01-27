160a0ffda998ef4de01cdb5f1dcb2a5347ed5e5c
Jared Hirsch <ohai@6a68.net>
2015-01-27 15:31:59 -0800

    typo

--- server/models/user-page.js
+++ server/models/user-page.js
@@ -57,7 +57,7 @@ var userPage = {
     ' = ' +
     '($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, ' +
     '$19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31) ' +
-    'WHERE user_id = $32 AND id = $33' +
+    'WHERE user_id = $32 AND id = $33 ' +
     'RETURNING *';
     var addDataParams = [data.extractedAt, data.extractedAuthorName, data.extractedAuthorUrl, 
       data.extractedCacheAge, data.extractedContent, data.extractedDescription,
