82043847baa5c4f08db7c8025218f9c44d3b8694
Jared Hirsch <ohai@6a68.net>
2015-02-04 11:43:14 -0800

    feat(db): add migrations support
    Fixes #92

---
+++ server/db/migrations/patch-0-1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE property (
+  key TEXT PRIMARY KEY,
+  value TEXT
+);
+INSERT INTO property(key, value) VALUES('patch', 1);
+
---
+++ server/db/migrations/patch-2-1.sql
@@ -0,0 +1,6 @@
+-- drop the visits table
+DROP TABLE visits;
+-- drop the user_pages table
+DROP TABLE user_pages;
+-- drop the users table
+DROP TABLE users;
