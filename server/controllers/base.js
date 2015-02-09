c3ce1e64b3bf26986f3ae06e331322b70e6d55db
Nick Chapman <nchapman@mozilla.com>
2015-02-09 09:07:42 -0800

    fix(server): Removes caching for root path.

--- server/controllers/base.js
+++ server/controllers/base.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+var fs = require('fs');
 var path = require('path');
 var config = require('../config');
 var STATIC_PATH = path.join(__dirname, '..', '..', config.get('server_staticPath'));
@@ -11,7 +12,14 @@ var STATIC_PATH = path.join(__dirname, '..', '..', config.get('server_staticPath
 var baseController = {
   get: function (request, reply) {
     var page = request.auth.isAuthenticated ? 'app.html' : 'index.html';
-    reply.file(path.join(STATIC_PATH, page));
+
+    fs.readFile(path.join(STATIC_PATH, page), 'utf8', function (err, data) {
+      if (err) {
+        throw err;
+      }
+
+      reply(data);
+    });
   }
 };
 
